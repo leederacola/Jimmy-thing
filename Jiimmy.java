c319d316eb5713b0013506ad7ab2535d12bc32da
leederacola <rmgforce@gmail.com>
2016-10-01 14:05:21 -0400

    Add files via upload
    i did it!

---
+++ Jiimmy.java
@@ -0,0 +1,22 @@
+/**
+ * read file name...... create substring title
+ * create directory one level up named title
+ * rename file game.iso
+ * 
+ */
+
+/**
+ *
+ * @author Ryan
+ */
+import java.io.*;
+
+public class Jiimmy
+{
+    public static void main (String[] args)
+    {
+        new File("C:\\Directory1").mkdir();
+        new File("C:\Users\\Ryan\\Desktop\\trial\\Work").mkdir();
+        C:\Users\Ryan\Desktop\trial\\Work"
+    }
+}
